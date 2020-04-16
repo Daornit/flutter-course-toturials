c5f2f36295dd15ab185b1c40c5c3ad4c756ab3f3
Daornit <daornit2@gmail.com>
2020-04-16 10:25:55 +0800

    package import, play sound by package, generate repeated interface Widgets by function, Dart function how i cna use it input as well, arrow function vs tranditional function

---
+++ project_4_xylophone-flutter/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\src\flt\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\batorgil\Desktop\Projects\flutter-course-toturials\project_4_xylophone-flutter"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\flt\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
